dff800d13f82a777b78be1fbe6dfe3d23030492f
Kim Kyungmin <qaws2051@gmail.com>
2021-11-19 14:34:09 +0900

    fix mistake

--- surveillance_system/Program.CCTV.cs
+++ surveillance_system/Program.CCTV.cs
@@ -1,175 +1,306 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Linq;
 
 namespace surveillance_system
 {
-   public partial class Program
+    public partial class Program
     {
-        public class Road
+        public class CCTV
         {
-            public double[] laneVector;
+            public int X;
 
-            // 가로 도로 좌표
-            // 첫번째 인덱스 = 도로 번호,  두번째 인덱스 = y 값
-            public double[,] lane_h; // 가로 - 중앙선 y 값 
-            public double[,] lane_h_upper; // 가로 - 중앙선 위 라인 y값 
-            public  double[,] lane_h_lower; // 가로 - 중앙선 아래 라인 y값 
+            public int Y;
 
-            // 세로 도로 좌표
-            // 첫번째 인덱스 = 도로 번호,  두번째 인덱스 = x 값
-            public double[,] lane_v; // 세로 - 중앙선 x값
-            public double[,] lane_v_left; // 세로- 중앙선 왼쪽 라인 x값
-            public double[,] lane_v_right; // 세로 중앙선 오른쪽 라인 x값
+            public int Z;
 
-            public double[,] DST; // 도로 교차점
+            public double WD;
 
-            public void roadBuilder(int wd, int intvl, int n_interval, int n_cctv, int n_ped)
+            public double HE;
+
+            public double H_AOV;
+
+            public double V_AOV;
+
+            public int imW;
+
+            public int imH;
+
+            public double Focal_Length;
+
+            public double ViewAngleH;
+
+            public double ViewAngleV;
+
+            public double MAX_Dist_X;
+
+            public double MAX_Dist_Y;
+
+            public double Direction;
+
+            public double[] H_FOV_X0;
+
+            public double[] H_FOV_X1;
+
+            public double[] H_FOV_X2;
+
+            public double[] H_FOV_Y0;
+
+            public double[] H_FOV_Y1;
+
+            public double[] H_FOV_Y2;
+
+            public double[] H_FOV_Z0;
+
+            public double[] H_FOV_Z1;
+
+            public double[] H_FOV_Z2;
+
+            public double[] V_FOV_X0;
+
+            public double[] V_FOV_X1;
+
+            public double[] V_FOV_X2;
+
+            public double[] V_FOV_Y0;
+
+            public double[] V_FOV_Y1;
+
+            public double[] V_FOV_Y2;
+
+            public double[] V_FOV_Z0;
+
+            public double[] V_FOV_Z1;
+
+            public double[] V_FOV_Z2;
+
+            public void get_H_FOV(
+                double[] Dist,
+                double HE,
+                double Focal_Length,
+                double ViewAngle,
+                double X,
+                double Y
+            )
             {
-                DST = new double[n_interval * n_interval, 2];
+                double[] H_FOV_0 = new double[Dist.Length];
+                double[] H_FOV_1 = new double[Dist.Length];
+                double[] H_FOV_2 = new double[Dist.Length];
 
-                // 교차점 좌표 저장
-                int idx = 0;
-                for (int i = 0; i < n_interval; i++)
+                double[] H_X0 = new double[Dist.Length];
+                double[] H_X1 = new double[Dist.Length];
+                double[] H_X2 = new double[Dist.Length];
+
+                double[] H_Y0 = new double[Dist.Length];
+                double[] H_Y1 = new double[Dist.Length];
+                double[] H_Y2 = new double[Dist.Length];
+
+                for (int i = 0; i < Dist.Length; i++)
                 {
-                    for (int j = 0; j < n_interval; j++)
-                    {
-                        DST[idx, 0] = (intvl + wd) * i;
-                        DST[idx, 1] = (intvl + wd) * j;
-                        idx++;
-                    }
+                    H_FOV_0[i] = 0;
+                    H_FOV_1[i] = (1 / 2) * Dist[i] * HE / Focal_Length;
+                    H_FOV_2[i] = (-1 / 2) * Dist[i] * HE / Focal_Length;
+
+                    H_X0[i] =
+                        Dist[i] * Math.Cos(ViewAngle) -
+                        H_FOV_0[i] * Math.Sin(ViewAngle);
+                    H_X1[i] =
+                        Dist[i] * Math.Cos(ViewAngle) -
+                        H_FOV_1[i] * Math.Sin(ViewAngle);
+                    H_X2[i] =
+                        Dist[i] * Math.Cos(ViewAngle) -
+                        H_FOV_2[i] * Math.Sin(ViewAngle);
+
+                    H_Y0[i] =
+                        Dist[i] * Math.Sin(ViewAngle) +
+                        H_FOV_0[i] * Math.Cos(ViewAngle);
+                    H_Y1[i] =
+                        Dist[i] * Math.Sin(ViewAngle) +
+                        H_FOV_1[i] * Math.Cos(ViewAngle);
+                    H_Y2[i] =
+                        Dist[i] * Math.Sin(ViewAngle) +
+                        H_FOV_2[i] * Math.Cos(ViewAngle);
                 }
+                double[,] H_FOV_X = new double[3, Dist.Length];
+                double[,] H_FOV_Y = new double[3, Dist.Length];
 
-                // 도로 벡터 초기화
-                double incr = 100;
-                int laneSize = (int)((intvl + wd) * (n_interval - 1) / incr);
-                //Console.WriteLine("laneSize = {0}", laneSize);
-                laneVector = new double[laneSize];
-
-                for (int i = 0; i < laneSize; i++)
+                for (int i = 0; i < Dist.Length; i++)
                 {
-                    laneVector[i] = i * incr;
+                    H_FOV_X[0, i] = H_X0[i] + X;
+                    H_FOV_X[1, i] = H_X1[i] + X;
+                    H_FOV_X[2, i] = H_X2[i] + X;
+                    H_FOV_Y[0, i] = H_Y0[i] + Y;
+                    H_FOV_Y[1, i] = H_Y1[i] + Y;
+                    H_FOV_Y[2, i] = H_Y2[i] + Y;
                 }
+                H_FOV_X0 = new double[Dist.Length];
+                H_FOV_X1 = new double[Dist.Length];
+                H_FOV_X2 = new double[Dist.Length];
 
-                // 가로 도로 좌표 설정
-                lane_h = new double[n_interval, 1];
-                lane_h_upper = new double[n_interval, 1];
-                lane_h_lower = new double[n_interval, 1];
+                H_FOV_Y0 = new double[Dist.Length];
+                H_FOV_Y1 = new double[Dist.Length];
+                H_FOV_Y2 = new double[Dist.Length];
 
-                for (int i = 0; i < n_interval; i++)
+                for (int i = 0; i < Dist.Length; i++)
                 {
-                    lane_h[i, 0] = i * (intvl + wd);
-                    lane_h_upper[i, 0] = lane_h[i, 0] + wd / 2;
-                    lane_h_lower[i, 0] = lane_h[i, 0] - wd / 2;
-                }
+                    H_FOV_X0[i] = H_FOV_X[0, i];
+                    H_FOV_X1[i] = H_FOV_X[1, i];
+                    H_FOV_X2[i] = H_FOV_X[2, i];
 
-                // 세로 도로 좌표 설정
-                lane_v = new double[n_interval, 1];
-                lane_v_left = new double[n_interval, 1];
-                lane_v_right = new double[n_interval, 1];
-                for (int i = 0; i < n_interval; i++)
-                {
-                    lane_v[i, 0] = i * (intvl + wd);
-                    lane_v_left[i, 0] = lane_h[i, 0] - wd / 2;
-                    lane_v_right[i, 0] = lane_h[i, 0] + wd / 2;
+                    H_FOV_Y0[i] = H_FOV_Y[0, i];
+                    H_FOV_Y1[i] = H_FOV_Y[1, i];
+                    H_FOV_Y2[i] = H_FOV_Y[2, i];
                 }
-
-                setCCTV(n_cctv,wd, n_interval);
-                setPed(n_ped);
             }
 
-            public void setPed(int n_ped)
+            public void get_V_FOV(
+                double[] Dist,
+                double WD,
+                double Focal_Length,
+                double ViewAngle,
+                double X,
+                double Z
+            )
             {
-                for(int i = 0; i < n_ped; i++)
-                {
-                    Random rand = new Random();
-                    double opt = rand.NextDouble();
+                double[] V_FOV_0 = new double[Dist.Length];
+                double[] V_FOV_1 = new double[Dist.Length];
+                double[] V_FOV_2 = new double[Dist.Length];
 
-                    if (opt > 0.5) {
-                        peds[i].X = Math.Round(laneVector.Max() * opt);
-                        peds[i].Y = lane_h[rand.Next(0, lane_h.GetLength(0)), 0];
-                    }
-                    else
-                    {
-                        peds[i].X =lane_v[rand.Next(0, lane_v.GetLength(0)), 0];
-                        peds[i].Y = Math.Round(laneVector.Max() * opt);
-                    }
+                double[] V_X0 = new double[Dist.Length];
+                double[] V_X1 = new double[Dist.Length];
+                double[] V_X2 = new double[Dist.Length];
+
+                double[] V_Y0 = new double[Dist.Length];
+                double[] V_Y1 = new double[Dist.Length];
+                double[] V_Y2 = new double[Dist.Length];
+
+                for (int i = 0; i < Dist.Length; i++)
+                {
+                    V_FOV_0[i] = 0;
+                    V_FOV_1[i] = (1 / 2) * Dist[i] * WD / Focal_Length;
+                    V_FOV_2[i] = (-1 / 2) * Dist[i] * WD / Focal_Length;
+
+                    V_X0[i] =
+                        Dist[i] * Math.Cos(ViewAngle) -
+                        V_FOV_0[i] * Math.Sin(ViewAngle);
+                    V_X1[i] =
+                        Dist[i] * Math.Cos(ViewAngle) -
+                        V_FOV_1[i] * Math.Sin(ViewAngle);
+                    V_X2[i] =
+                        Dist[i] * Math.Cos(ViewAngle) -
+                        V_FOV_2[i] * Math.Sin(ViewAngle);
+
+                    V_Y0[i] =
+                        Dist[i] * Math.Sin(ViewAngle) +
+                        V_FOV_0[i] * Math.Cos(ViewAngle);
+                    V_Y1[i] =
+                        Dist[i] * Math.Sin(ViewAngle) +
+                        V_FOV_1[i] * Math.Cos(ViewAngle);
+                    V_Y2[i] =
+                        Dist[i] * Math.Sin(ViewAngle) +
+                        V_FOV_2[i] * Math.Cos(ViewAngle);
+                }
+                double[,] V_FOV_X = new double[3, Dist.Length];
+                double[,] V_FOV_Y = new double[3, Dist.Length];
+
+                for (int i = 0; i < Dist.Length; i++)
+                {
+                    V_FOV_X[0, i] = V_X0[i] + X;
+                    V_FOV_X[1, i] = V_X1[i] + X;
+                    V_FOV_X[2, i] = V_X2[i] + X;
+                    V_FOV_Y[0, i] = V_Y0[i] + Z;
+                    V_FOV_Y[1, i] = V_Y1[i] + Z;
+                    V_FOV_Y[2, i] = V_Y2[i] + Z;
+                }
+                V_FOV_X0 = new double[Dist.Length];
+                V_FOV_X1 = new double[Dist.Length];
+                V_FOV_X2 = new double[Dist.Length];
+
+                V_FOV_Y0 = new double[Dist.Length];
+                V_FOV_Y1 = new double[Dist.Length];
+                V_FOV_Y2 = new double[Dist.Length];
+
+                for (int i = 0; i < Dist.Length; i++)
+                {
+                    V_FOV_X0[i] = V_FOV_X[0, i];
+                    V_FOV_X1[i] = V_FOV_X[1, i];
+                    V_FOV_X2[i] = V_FOV_X[2, i];
+
+                    V_FOV_Y0[i] = V_FOV_Y[0, i];
+                    V_FOV_Y1[i] = V_FOV_Y[1, i];
+                    V_FOV_Y2[i] = V_FOV_Y[2, i];
                 }
             }
 
-            public void setCCTV(int n_cctv, int wd, int n_interval)
+
+            // public double Angle;
+            /* ==================================
+            /   추가 line 45~ 51 변수 ,  get_PixelDensity 함수 (깃허브 get_PixelDensity.m)
+            / ===================================*/
+            public double[] SurvDist_H;
+
+            public double[] SurvDist_V;
+
+            public double[] PPM_H;
+
+            public double[] PPM_V;
+
+            public void get_PixelDensity(
+                double[] dist,
+                double WD,
+                double HE,
+                double Lens_FocalLength,
+                int imW,
+                int imH
+            )
             {
-                for (int i = 0; i < n_cctv; i++)
+                int len = dist.Length;
+                double[] Dist_Meter = new double[len];
+                double[] H_FOV = new double[len];
+                double[] V_FOV = new double[len];
+                PPM_H = new double[len];
+                PPM_V = new double[len];
+
+                for (int i = 0; i < len; i++)
                 {
-                    Random rand = new Random();
-                    double opt = rand.NextDouble();
-                    
-                    // 잘 모르겠음 
-                    if (opt > 0.5)
+                    Dist_Meter[i] = dist[i] * 0.003;
+                }
+
+                for (int i = 0; i < len; i++)
+                {
+                    H_FOV[i] = Dist_Meter[i] * WD / Lens_FocalLength;
+                    V_FOV[i] = Dist_Meter[i] * HE / Lens_FocalLength;
+                }
+
+                for (int i = 0; i < len; i++)
+                {
+                    PPM_H[i] = imW / H_FOV[i];
+                    PPM_V[i] = imH / V_FOV[i];
+                }
+
+                int[] List_PPM_Criteria = new int[] { 250, 125, 62, 25, 12 };
+                SurvDist_H = new double[List_PPM_Criteria.Length];
+                SurvDist_V = new double[List_PPM_Criteria.Length];
+
+                for (int i = 0; i < List_PPM_Criteria.Length; i++)
+                {
+                    double[] temp = new double[len];
+                    for (int j = 0; j < len; j++)
                     {
-                        double rand_y = Math.Round(opt);
-                        if (rand_y == 0) rand_y = -1;
-
-                        cctvs[i].X = (int)(-1 * wd / 2 + laneVector.Max() * opt);
-                        cctvs[i].Y = (int)(lane_h[rand.Next(0, n_interval),0]+rand_y*wd/2);
+                        temp[j] = Math.Abs(PPM_H[j] - List_PPM_Criteria[i]);
                     }
-                    else
+
+                    int idx = findMinIdx(temp);
+                    SurvDist_H[i] = Dist_Meter[idx] * 0.003;
+
+                    for (int j = 0; j < len; j++)
                     {
-                        double rand_x = Math.Round(opt);
-                        if (rand_x == 0) rand_x = -1;
-
-                        cctvs[i].X = (int)(lane_v[rand.Next(0, n_interval), 0] + rand_x * wd / 2);
-                        cctvs[i].Y = (int)(-1 * wd / 2 + laneVector.Max() * opt);
+                        temp[j] = Math.Abs(PPM_V[j] - List_PPM_Criteria[i]);
                     }
+
+                    idx = findMinIdx(temp);
+                    SurvDist_V[i] = Dist_Meter[idx] * 0.003;
                 }
-            }
-
-            public void printRoadInfo()
-            {
-                Console.WriteLine("\n======================== DST ===============================================");
-                for (int i = 0; i < DST.GetLength(0); i++)
-                {
-                    for (int j = 0; j < 2; j++)
-                    {
-                        if (j == 0) Console.Write("DST[{0}].X = {1}      ", i, DST[i, j]);
-                        if (j == 1) Console.Write("DST[{0}].Y = {1}      ", i, DST[i, j]);
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine("\n===========================================================================\n");
-
-                /* -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-                   Console.WriteLine("\n========================lane Vector========================================");
-                   for (int i = 0; i < laneVector.Length; i++)
-                   {
-                       Console.Write(laneVector[i] + "  ");
-                       if (i % 99 == 0) Console.WriteLine();
-                   }
-                   Console.WriteLine("\n===========================================================================\n");
-
-                ------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
-                Console.WriteLine("\n========================lane horizontal======================================");
-                for (int i = 0; i < lane_h.GetLength(0); i++)
-                {
-                    Console.WriteLine("\n{0}번째 가로 도로 정보", i);
-                    Console.WriteLine("y좌표 : 위 - 중앙 - 아래");
-                    Console.WriteLine("       {0}   {1}   {2}", lane_h_upper[i,0],lane_h[i,0],lane_h_lower[i,0]);
-                }
-                Console.WriteLine("\n===========================================================================\n");
-
-                
-
-                Console.WriteLine("\n========================lane vertical========================================");
-                for (int i = 0; i < lane_h.GetLength(0); i++)
-                {
-                    Console.WriteLine("\n{0}번째 세로 도로 정보", i);
-                    Console.WriteLine("x좌표 : 왼쪽 - 중앙 - 오른쪽");
-                    Console.WriteLine("       {0}   {1}   {2}", lane_v_left[i, 0], lane_v[i, 0], lane_v_right[i, 0]);
-                }
-                Console.WriteLine("\n===========================================================================\n");
             }
         }
     }
--- surveillance_system/Program.Road.cs
+++ surveillance_system/Program.Road.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace surveillance_system
 {
@@ -8,21 +9,21 @@ namespace surveillance_system
     {
         public class Road
         {
-            double[] laneVector;
+            public double[] laneVector;
 
             // 가로 도로 좌표
             // 첫번째 인덱스 = 도로 번호,  두번째 인덱스 = y 값
-            double[,] lane_h; // 가로 - 중앙선 y 값 
-            double[,] lane_h_upper; // 가로 - 중앙선 위 라인 y값 
-            double[,] lane_h_lower; // 가로 - 중앙선 아래 라인 y값 
+            public double[,] lane_h; // 가로 - 중앙선 y 값 
+            public double[,] lane_h_upper; // 가로 - 중앙선 위 라인 y값 
+            public  double[,] lane_h_lower; // 가로 - 중앙선 아래 라인 y값 
 
             // 세로 도로 좌표
             // 첫번째 인덱스 = 도로 번호,  두번째 인덱스 = x 값
-            double[,] lane_v; // 세로 - 중앙선 x값
-            double[,] lane_v_left; // 세로- 중앙선 왼쪽 라인 x값
-            double[,] lane_v_right; // 세로 중앙선 오른쪽 라인 x값
+            public double[,] lane_v; // 세로 - 중앙선 x값
+            public double[,] lane_v_left; // 세로- 중앙선 왼쪽 라인 x값
+            public double[,] lane_v_right; // 세로 중앙선 오른쪽 라인 x값
 
-            double[,] DST; // 도로 교차점
+            public double[,] DST; // 도로 교차점
 
             public void roadBuilder(int wd, int intvl, int n_interval, int n_cctv, int n_ped)
             {
@@ -41,8 +42,9 @@ namespace surveillance_system
                 }
 
                 // 도로 벡터 초기화
-                double incr = 0.01;
+                double incr = 100;
                 int laneSize = (int)((intvl + wd) * (n_interval - 1) / incr);
+                //Console.WriteLine("laneSize = {0}", laneSize);
                 laneVector = new double[laneSize];
 
                 for (int i = 0; i < laneSize; i++)
@@ -57,9 +59,9 @@ namespace surveillance_system
 
                 for (int i = 0; i < n_interval; i++)
                 {
-                    lane_h[i, 1] = i * (intvl + wd);
-                    lane_h_upper[i, 1] = lane_h[i, 1] + wd / 2;
-                    lane_h_lower[i, 1] = lane_h[i, 1] - wd / 2;
+                    lane_h[i, 0] = i * (intvl + wd);
+                    lane_h_upper[i, 0] = lane_h[i, 0] + wd / 2;
+                    lane_h_lower[i, 0] = lane_h[i, 0] - wd / 2;
                 }
 
                 // 세로 도로 좌표 설정
@@ -68,10 +70,106 @@ namespace surveillance_system
                 lane_v_right = new double[n_interval, 1];
                 for (int i = 0; i < n_interval; i++)
                 {
-                    lane_v[i, 1] = i * (intvl + wd);
-                    lane_v_left[i, 1] = lane_h[i, 1] - wd / 2;
-                    lane_v_right[i, 1] = lane_h[i, 1] + wd / 2;
+                    lane_v[i, 0] = i * (intvl + wd);
+                    lane_v_left[i, 0] = lane_h[i, 0] - wd / 2;
+                    lane_v_right[i, 0] = lane_h[i, 0] + wd / 2;
                 }
+
+                setCCTV(n_cctv,wd, n_interval);
+                setPed(n_ped);
+            }
+
+            public void setPed(int n_ped)
+            {
+                for(int i = 0; i < n_ped; i++)
+                {
+                    Random rand = new Random();
+                    double opt = rand.NextDouble();
+
+                    if (opt > 0.5) {
+                        peds[i].X = Math.Round(laneVector.Max() * opt);
+                        peds[i].Y = lane_h[rand.Next(0, lane_h.GetLength(0)), 0];
+                    }
+                    else
+                    {
+                        peds[i].X =lane_v[rand.Next(0, lane_v.GetLength(0)), 0];
+                        peds[i].Y = Math.Round(laneVector.Max() * opt);
+                    }
+                }
+            }
+
+            public void setCCTV(int n_cctv, int wd, int n_interval)
+            {
+                for (int i = 0; i < n_cctv; i++)
+                {
+                    Random rand = new Random();
+                    double opt = rand.NextDouble();
+                    
+                    // 잘 모르겠음 
+                    if (opt > 0.5)
+                    {
+                        double rand_y = Math.Round(opt);
+                        if (rand_y == 0) rand_y = -1;
+
+                        cctvs[i].X = (int)(-1 * wd / 2 + laneVector.Max() * opt);
+                        cctvs[i].Y = (int)(lane_h[rand.Next(0, n_interval),0]+rand_y*wd/2);
+                    }
+                    else
+                    {
+                        double rand_x = Math.Round(opt);
+                        if (rand_x == 0) rand_x = -1;
+
+                        cctvs[i].X = (int)(lane_v[rand.Next(0, n_interval), 0] + rand_x * wd / 2);
+                        cctvs[i].Y = (int)(-1 * wd / 2 + laneVector.Max() * opt);
+                    }
+                }
+            }
+
+            public void printRoadInfo()
+            {
+                Console.WriteLine("\n======================== DST ===============================================");
+                for (int i = 0; i < DST.GetLength(0); i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        if (j == 0) Console.Write("DST[{0}].X = {1}      ", i, DST[i, j]);
+                        if (j == 1) Console.Write("DST[{0}].Y = {1}      ", i, DST[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("\n===========================================================================\n");
+
+                /* -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+                   Console.WriteLine("\n========================lane Vector========================================");
+                   for (int i = 0; i < laneVector.Length; i++)
+                   {
+                       Console.Write(laneVector[i] + "  ");
+                       if (i % 99 == 0) Console.WriteLine();
+                   }
+                   Console.WriteLine("\n===========================================================================\n");
+
+                ------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+                Console.WriteLine("\n========================lane horizontal======================================");
+                for (int i = 0; i < lane_h.GetLength(0); i++)
+                {
+                    Console.WriteLine("\n{0}번째 가로 도로 정보", i);
+                    Console.WriteLine("y좌표 : 위 - 중앙 - 아래");
+                    Console.WriteLine("       {0}   {1}   {2}", lane_h_upper[i,0],lane_h[i,0],lane_h_lower[i,0]);
+                }
+                Console.WriteLine("\n===========================================================================\n");
+
+                
+
+                Console.WriteLine("\n========================lane vertical========================================");
+                for (int i = 0; i < lane_h.GetLength(0); i++)
+                {
+                    Console.WriteLine("\n{0}번째 세로 도로 정보", i);
+                    Console.WriteLine("x좌표 : 왼쪽 - 중앙 - 오른쪽");
+                    Console.WriteLine("       {0}   {1}   {2}", lane_v_left[i, 0], lane_v[i, 0], lane_v_right[i, 0]);
+                }
+                Console.WriteLine("\n===========================================================================\n");
             }
         }
     }
