32d24b809a1c019e0fc45fdce6b7709f3d3a4cae
Kim Kyungmin <qaws2051@gmail.com>
2022-01-25 00:30:52 +0900

    merge yk and km

---
+++ 2021-2_SurveillanceSystem/2021-2_SurveillanceSystem-km/surveillance_system/src/Program.Math.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace surveillance_system
+{
+    public partial class Program
+    {
+        public static double InnerProduct(double[] a, double[] b)
+        {
+            if (a.Length != b.Length) return 0;
+
+            double acc = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                acc += a[i] * b[i];
+            }
+            return acc;
+        }
+
+        public static double Norm(double[] a)
+        {
+            return Math.Sqrt(a[0] * a[0] + a[1] * a[1]);
+        }
+
+        public static double RadToDeg(double angle)
+        {
+            return (180 / Math.PI) * angle;
+        }
+
+        public static double DegToRad(double angle)
+        {
+            return (Math.PI / 180) * angle;
+        }
+    }
+
+}
